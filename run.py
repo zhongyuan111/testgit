bdbf2d2baf252178e861f6643bdc2ce832347d54
liujiangnan <695541723@qq.com>
2021-10-08 15:14:44 +0800

    修改验证

--- run.py
+++ run.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # coding=gbk
-import pytest
 import os
+import pytest
 
 root_dir = os.path.dirname(os.path.abspath('.'))  # 获取项目根目录的相对路径
-file_path = root_dir + '\\auto_4.0\\Report'
+file_path = root_dir + '\\UiTest\\Report'
 print('root_dir:::',root_dir)
 
 if __name__ == "__main__":
     #pytest.main(['-s'])
     # 执行pytest单元测试，生成 Allure 报告需要的数据存在 /temp 目录
     #pytest.main(['--alluredir', file_path])
-    pytest.main(['-s', '-q', '--alluredir', file_path, root_dir+'\\auto_4.0\\Testcase\\Tenant\\1Super_agent'])
+    pytest.main(['-s', '-v', '--alluredir', file_path, root_dir + '\\UiTest\\Testcase\\test_callmange'])
+    #分布式执行
+    #pytest.main(['-s', '-v','-n','auto','--dist','loadfile','--alluredir', file_path, root_dir+'\\UiTest\\Testcase\\test_callrobot'])
     # 执行命令 allure generate ./temp -o ./report --clean ，生成测试报告
-    #os.popen('allure generate --clean ./Report')
-    os.popen('allure generate ./Report  -o ./allure-report   --clean')
-
-
-
+    #os.system("allure generate report -o report/html")
+    os.popen('allure generate D:\\demo\\UiTest\\Report  -o D:\\demo\\UiTest\\allure-report   --clean')
+    #os.popen('allure generate ./Report  -o ./allure-report   --clean')
